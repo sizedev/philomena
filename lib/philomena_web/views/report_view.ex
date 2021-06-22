661eacad3ccc9eb3ce82e5abe470e80944d2371a
Natalie Fearnley <nfearnley@gmail.com>
2021-06-22 02:27:29 -0400

    cleanup rules

--- lib/philomena_web/views/report_view.ex
+++ lib/philomena_web/views/report_view.ex
@@ -13,17 +13,9 @@ defmodule PhilomenaWeb.ReportView do
 
   def report_categories do
     [
-      "Rule #0: Namecalling, trolling, discrimination": "Rule #0",
-      "Rule #1: DNP, content theft, pay content, trace/bad edit": "Rule #1",
-      "Rule #2: Bad tagging/sourcing": "Rule #2",
-      "Rule #3: Image not MLP-related/obligatory pony": "Rule #3",
-      "Rule #4: Whining about filterable content": "Rule #4",
-      "Rule #5: Underage+human/anthro-looking porn": "Rule #5",
-      "Rule #6: Spam, off-topic, or general site abuse": "Rule #6",
-      "Rule #7: Above topic rating (NOT swear words)": "Rule #7",
-      "Rule #8: Privacy violation": "Rule #8",
-      "Rule #9: Commissions": "Rule #9",
-      "Rule #n: Spirit of the rules": "Rule #n",
+      "Rule #0: All members must be 18 or older": "Rule #0",
+      "Rule #1: Don't be an asshole": "Rule #1",
+      "Rule #2: Properly tag NSFW content": "Rule #2",
       "Other (please explain)": "Other",
       "Takedown request": "Takedown request"
     ]
